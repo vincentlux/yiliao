cd0ac5129ca140105e9ada91f74f2d2a704fe6b5
Xuanqiang Zhao <vsastray@outlook.com>
2019-08-05 17:17:35 +0900

    login page css

--- yiliao/src/login/LoginPage.js
+++ yiliao/src/login/LoginPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Redirect } from 'react-router-dom';
-import { Button, Form, FormGroup, Label, Input, Alert, Spinner } from 'reactstrap';
+import { Alert, Spinner } from 'reactstrap';
+import './Login.css'
 
 export default class LoginPage extends React.Component {
     constructor(props) {
@@ -50,23 +51,20 @@ export default class LoginPage extends React.Component {
 
     render() {
         return (
-            <div>
-                <Form onSubmit={this.handleSubmit} className="login">
-                    <FormGroup>
-                        <Label>用户名</Label>
-                        <Input onChange={(e) => {this.setState({"username": e.target.value})}} value={this.state.username}/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label>密码</Label>
-                        <Input type="password" onChange={(e) => {this.setState({"password": e.target.value})}} value={this.state.password} />
-                    </FormGroup>
-                    <Button className="loginButton">
+            <div id="loginBox">
+                <form onSubmit={this.handleSubmit} className="login">
+                    <h1 id="loginTitle">用户登录</h1>
+                    <label className="loginLabel">用户名</label><br />
+                    <input className="loginInput" type="text" onChange={(e) => {this.setState({"username": e.target.value})}} value={this.state.username}/><br />
+                    <label className="loginLabel">密码</label><br />
+                    <input className="loginInput" type="password" onChange={(e) => {this.setState({"password": e.target.value})}} value={this.state.password} />
+                    <button id="loginButton">
                         <span>登录</span>
-                    </Button>
-                </Form>
-                {this.state.loading?<Spinner color="primary" />:''}
+                    </button>
+                </form>
+                {this.state.loading?<Spinner color="secondary" />:''}
                 {this.state.login==='failure'?<Alert color='danger'>用户名或密码不正确</Alert>:
-                    this.state.login==='success'?<Redirect to={{ pathname: "/user", search: "?name="+this.state.username, state: {username: this.state.username} }} />:''}
+                    this.state.login==='success'?<Redirect push to={{ pathname: "/user", search: "?name="+this.state.username, state: {username: this.state.username} }} />:''}
             </div>
 
         );
